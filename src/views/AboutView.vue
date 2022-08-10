a711cc1ad25a5d70db27976e96ea4dd44e4ef35f
Leandro Cavalcanti <leandro360cavalcanti@gmail.com>
2022-08-09 23:31:34 -0300

    Setup sample app

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
+  <div class="about pa-16">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Dolore mollitia eaque cupiditate corporis tenetur laborum iure qui expedita id nam, 
+      quos aspernatur quod, vel sit saepe, eligendi odit commodi aut?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Dolore mollitia eaque cupiditate corporis tenetur laborum iure qui expedita id nam, 
+      quos aspernatur quod, vel sit saepe, eligendi odit commodi aut?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Dolore mollitia eaque cupiditate corporis tenetur laborum iure qui expedita id nam, 
+      quos aspernatur quod, vel sit saepe, eligendi odit commodi aut?
+    </p>
   </div>
 </template>
